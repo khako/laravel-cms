dc44a4e5136416db29e2960b66924a7396bf0df6
khako <k.lahbibi@gmail.com>
2017-07-13 22:25:45 +0200

    layout setup

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
+});
+
+Route::get('/blog/first', function () {
+    return view('blog.first');
 });
